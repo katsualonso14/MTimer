f24be1518764f1f3270245fd911e1e18bbdf9b4b
ktsilva21 <61615140+katsualonso14@users.noreply.github.com>
2020-06-22 23:17:24 +0900

    Add files via upload

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -52,6 +52,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
+//        アプリがスリープにならない
+        UIApplication.shared.isIdleTimerDisabled = true
+        
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
